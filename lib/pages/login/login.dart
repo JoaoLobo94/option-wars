061f2ec8752bb876b7f2f3769e71304cbf1df6b5
Joao Lobo <joao.lobom@gmail.com>
2023-10-28 15:13:36 +0100

    login screen completion

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -9,7 +9,6 @@ class Login extends StatelessWidget {
     return Scaffold(
         body: Container(
           width: double.infinity,
-          padding: const EdgeInsets.symmetric(vertical: 20),
           decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
@@ -23,80 +22,70 @@ class Login extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Login", style: TextStyle(color: Colors.white, fontSize: 40)),
+                    FadeAnimation()Text("Welcome back", style: TextStyle(color: Colors.white, fontSize: 40)),
                     SizedBox(height: 10),
-                    Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18)),
+                    Text("The battle awaits you", style: TextStyle(color: Colors.white, fontSize: 18)),
                   ],
                 ),
               ),
               const SizedBox(height: 20),
-              Expanded(child: Container(
-                decoration: const BoxDecoration(
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(height: 60),
-                      LoginTextInput(labelText: "Email"),
-                      const SizedBox(height: 20),
-                      LoginTextInput(labelText: "Password"),
-                      const SizedBox(height: 20),
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        alignment: Alignment.centerRight,
-                        child: const Text("Forgot Password?", style: TextStyle(color: Colors.orange, fontSize: 14)),
-                      ),
-                      const SizedBox(height: 40),
-                      Container(
-                        height: 50,
-                        margin: const EdgeInsets.symmetric(horizontal: 50),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.orange[900]
-                        ),
-                        child: const Center(
-                          child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
-                        ),
-                      ),
-                      const SizedBox(height: 50),
-                      const Text("Continue with social media", style: TextStyle(color: Colors.grey)),
-                      const SizedBox(height: 30),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.blue
-                              ),
-                              child: const Center(
-                                child: Text("Facebook", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
-                              ),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60)),
+                  ),
+                  child:  Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget> [
+                            const SizedBox(height:60),
+                            Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: const [BoxShadow(
+                                        color: Color.fromRGBO(225, 95, 27, .3),
+                                        blurRadius: 20,
+                                        offset: Offset(0, 10)
+                                    )]
+                                ),
+                                child: Column(
+                                    children: <Widget>[
+                                      Container(
+                                          padding: EdgeInsets.all(10),
+                                          decoration: BoxDecoration(
+                                            border: Border(bottom: BorderSide(color: Colors.grey[200]!)),
+                                          ),
+                                          child: const TextField(
+                                              decoration: InputDecoration(
+                                                  hintText: "Enter your Npriv key",
+                                                  hintStyle: TextStyle(color: Colors.grey),
+                                                  border: InputBorder.none))
+                                      ),
+                                    ]
+                                )
                             ),
-                          ),
-                          const SizedBox(width: 30),
-                          Expanded(
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.black
-                              ),
-                              child: const Center(
-                                child: Text("Github", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
-                              ),
+                            SizedBox(height: 40),
+                            Container(
+                                height: 50,
+                                margin: EdgeInsets.symmetric(horizontal:50),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.orange[900]!
+                                ),
+                                child: const Center(
+                                    child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold))
+                                )
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 40),
+                            Text("Generate Npriv key",style: TextStyle(color: Colors.grey)),
+                          ]
                       )
-                    ],
                   ),
                 ),
-              )),
+              ),
             ],
           ),
         ));
