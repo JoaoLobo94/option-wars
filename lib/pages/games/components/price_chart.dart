ec7ad8ea346ca40e6fa2133ba10d4fbbfd171bf8
Joao Lobo <joaolobo@Joaos-MacBook-Pro.local>
2023-11-12 16:36:09 +0000

    finish line chart

--- lib/pages/games/components/price_chart.dart
+++ lib/pages/games/components/price_chart.dart
@@ -5,7 +5,7 @@ class PriceChart extends StatelessWidget {
   final List<double> priceData;
   final List<double> firstPrice;
 
-  const PriceChart({super.key, required this.priceData, required this.firstPrice});
+  const PriceChart({Key? key, required this.priceData, required this.firstPrice}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +20,95 @@ class PriceChart extends StatelessWidget {
             spots: List.generate(priceData.length, (index) {
               return FlSpot(index.toDouble(), priceData[index]);
             }),
+            dotData: const FlDotData(
+              show: false,
+            ),
             isCurved: true,
-            color: Colors.greenAccent,
-            belowBarData: BarAreaData(show: true, color: Colors.greenAccent.withOpacity(0.5)),
+            color: _getColor(priceData.last, firstPrice.first, false),
+            belowBarData: BarAreaData(show: true, color: _getColor(priceData.last, firstPrice.first, true), ),
           ),
+
+
         ],
         extraLinesData: ExtraLinesData(
+          extraLinesOnTop: true,
           horizontalLines: [
             HorizontalLine(
               y: firstPrice[0],
               color: Colors.red,
               strokeWidth: 2,
               dashArray: [5, 2],
+              label: HorizontalLineLabel(
+                show: true,
+                alignment: Alignment.topRight,
+                padding: const EdgeInsets.only(right: 5, bottom: 5),
+                style: const TextStyle(
+                  fontSize: 9,
+                  fontWeight: FontWeight.bold,
+                ),
+                labelResolver: (line) => 'Start Price: ${line.y}',
+              ),
             ),
           ],
         ),
+        gridData: FlGridData(show: true,
+          drawVerticalLine: false,
+          drawHorizontalLine: true,
+          horizontalInterval: 0.5, // Set the interval for horizontal lines
+          getDrawingHorizontalLine: (value) {
+            return const FlLine(
+              color: Colors.grey,
+              strokeWidth: 1,
+              dashArray: [5, 2],
+            );
+          },
+        ),
+        titlesData: FlTitlesData(show: true,
+          topTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          rightTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          bottomTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          leftTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 46,
+                getTitlesWidget: leftTitleWidgets,
+              ),
+          )
+        ),
       ),
+    );
+  }
+
+
+  Color _getColor(double priceData, double firstPrice, bool hasOpacity) {
+    if (priceData > firstPrice) {
+      return hasOpacity ? Colors.green.withOpacity(0.2) : Colors.greenAccent;
+    } else if (priceData < firstPrice) {
+      return hasOpacity ? Colors.red.withOpacity(0.2) : Colors.redAccent;
+    } else {
+      return hasOpacity ? Colors.blue.withOpacity(0.2) : Colors.blueAccent;
+    }
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+     TextStyle style = TextStyle(
+      color: _getColor(value,firstPrice[0], false),
+      fontSize: 10,
+    );
+
+    String text = '${value}';
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 5,
+      fitInside: SideTitleFitInsideData.fromTitleMeta(meta),
+      child: Text(text, style: style, textAlign: TextAlign.center),
     );
   }
 }
